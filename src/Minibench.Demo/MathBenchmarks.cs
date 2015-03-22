ee53ad7dcb3932e650659fad8858c185a81ce446
Jon Skeet <skeet@pobox.com>
2015-03-22 16:39:07 +0000

    Add demo app (also used to debug :) Need to write some actual tests some time...

---
+++ src/Minibench.Demo/MathBenchmarks.cs
@@ -0,0 +1,23 @@
+﻿// Copyright 2015 The Minibench Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+using Minibench.Framework;
+using System;
+
+namespace Minibench.Demo
+{
+    public class MathBenchmarks
+    {
+        [Benchmark]
+        public void Sqrt()
+        {
+            Math.Sqrt(2.0).Consume();
+        }
+
+        [Benchmark]
+        public void Abs_Int32()
+        {
+            Math.Abs(-5).Consume();
+        }
+    }
+}
---
+++ src/Minibench.Demo/Program.cs
@@ -0,0 +1,18 @@
+﻿// Copyright 2015 The Minibench Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+using Minibench.Framework;
+
+namespace Minibench.Demo
+{
+    /// <summary>
+    /// Small example to show how Minibench can be used.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.RunFromCommandLine(typeof(Program).Assembly);
+        }
+    }
+}
